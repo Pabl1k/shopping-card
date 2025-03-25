9735c3328386f75f34bc7805136e46ba090cc84e
Pabl1k <poiumazaya@gmail.com>
2025-03-25 13:16:19 +0200

    svgr and clsx installed, removed empty files, added declaration.d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: { exportType: 'named', ref: true, svgo: false, titleProp: true },
+      include: '**/*.svg'
+    }),
+    tailwindcss()
+  ]
 });
