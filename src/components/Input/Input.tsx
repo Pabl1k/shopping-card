1c1113e570681755e775e02454247c5fb5cf3f54
Pabl1k <poiumazaya@gmail.com>
2025-03-26 12:17:14 +0200

    simplified input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import clsx from 'clsx';
-import './Input.css';
 
 interface Props {
   className?: string;
@@ -10,11 +9,21 @@ interface Props {
 
 const Input: FC<Props> = ({ className, label, value }) => {
   return (
-    <div className={clsx(className, 'inputGroup')}>
-      <input required autoComplete="off" value={value} />
-      <label htmlFor={label}>{label}</label>
+    <div className={clsx(className, 'relative w-full')}>
+      <input
+        value={value}
+        className="w-full h-full rounded-input border border-border p-3 leading-normal outline-none focus:border-border-hover placeholder:text-text-grey"
+        placeholder={label}
+      />
     </div>
   );
 };
 
 export default Input;
+
+// import './Input.css';
+//
+// <div className={clsx(className, 'inputGroup')}>
+//       <input required autoComplete="off" value={value} />
+//       <label htmlFor={label}>{label}</label>
+//     </div>
