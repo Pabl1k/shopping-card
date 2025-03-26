02fb87354c811ced42c70a86a38963188f74706f
Pabl1k <poiumazaya@gmail.com>
2025-03-26 13:23:03 +0200

    Payment section

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,14 +1,19 @@
 import { FC, ReactNode } from 'react';
+import clsx from 'clsx';
 
 interface Props {
   title: string;
+  description?: string;
   children: ReactNode;
 }
 
-const Wrapper: FC<Props> = ({ title, children }) => {
+const Wrapper: FC<Props> = ({ title, description, children }) => {
   return (
     <div className="bg-background-white flex flex-col py-3">
-      <span className="text-large font-bold leading-large mb-3">{title}</span>
+      <span className={clsx(description ? 'mb-2' : 'mb-3', 'text-large font-bold leading-large')}>
+        {title}
+      </span>
+      {description && <span className="text-small text-text-grey mb-2">{description}</span>}
       {children}
     </div>
   );
--- src/containers/Payment.tsx
+++ src/containers/Payment.tsx
@@ -1,22 +1,40 @@
 import { FC } from 'react';
 import Input from '../components/Input/Input.tsx';
 import Wrapper from '../components/Wrapper.tsx';
+import { ReactComponent as Visa } from '../assets/icons/visa.svg';
+import { ReactComponent as MasterCard } from '../assets/icons/mastercard.svg';
+import { ReactComponent as Amex } from '../assets/icons/amex.svg';
+import { ReactComponent as Diners } from '../assets/icons/diners.svg';
 
 interface Props {
   onChange?: () => void;
 }
 
 const Payment: FC<Props> = () => {
+  const paymentMethods = [<Visa />, <MasterCard />, <Amex />, <Diners />];
+
   return (
-    <Wrapper title="Payment">
-      <span className="text-small text-text-grey">All transactions are secure and encrypted.</span>
-      <div className="flex flex-col gap-3">
-        <Input label="Card number" value="" />
-        <div className="flex gap-3">
-          <Input label="Expiration (MM/YY)" value="" />
-          <Input label="Security code" value="" />
+    <Wrapper title="Payment" description="All transactions are secure and encrypted.">
+      <div className="flex flex-col border border-border rounded-input">
+        <div className="h-[56px] px-3 flex items-center justify-between bg-background-light-blue border border-border-blue rounded-tl-input rounded-tr-input">
+          <div className="flex items-center">
+            <input type="radio" checked className="size-[20px]" />
+            <span className="ml-2">Credit Card</span>
+          </div>
+          <div className="flex gap-1">
+            {paymentMethods.map((method, index) => (
+              <div key={index}>{method}</div>
+            ))}
+          </div>
         </div>
-        <Input label="Name on card" value="" />
+        <div className="flex flex-col p-3 gap-3 bg-background-light-grey">
+          <Input label="Card number" value="" />
+          <div className="flex gap-3">
+            <Input label="Expiration (MM/YY)" value="" />
+            <Input label="Security code" value="" />
+          </div>
+          <Input label="Name on card" value="" />
+        </div>
       </div>
     </Wrapper>
   );
