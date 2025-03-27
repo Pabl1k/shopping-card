802a0329d3b58603a23187f9cdb14a1d92417d5b
Pabl1k <poiumazaya@gmail.com>
2025-03-27 12:47:07 +0200

    dropdown icon

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,15 +1,19 @@
 import { FC, useRef, useState } from 'react';
 import Input from './Input/Input.tsx';
 import { useOutsideClick } from '../common/useOutsideClick.ts';
+import { ReactComponent as ArrowIcon } from '../assets/icons/arrow.svg';
+import clsx from 'clsx';
 
 interface Props {
   label: string;
   value: string;
   options: string[];
+  disabled?: boolean;
   onSelect: (value: string) => void;
+  onReset: () => void;
 }
 
-const Dropdown: FC<Props> = ({ label, value, options, onSelect }) => {
+const Dropdown: FC<Props> = ({ label, value, options, disabled = false, onSelect, onReset }) => {
   const [open, setOpen] = useState(false);
 
   const ref = useRef<HTMLDivElement>(null);
@@ -21,17 +25,40 @@ const Dropdown: FC<Props> = ({ label, value, options, onSelect }) => {
 
   useOutsideClick(ref, () => setOpen(false));
 
+  const renderPrefix = () => {
+    if (value) {
+      return (
+        <button
+          className="mr-3 cursor-pointer size-[20px]"
+          onClick={(e) => {
+            e.stopPropagation();
+            onReset();
+          }}
+        >
+          x
+        </button>
+      );
+    }
+
+    return (
+      <ArrowIcon
+        className={clsx(!open && 'rotate-180', 'transform transition-transform', 'mr-3')}
+      />
+    );
+  };
+
   return (
     <div ref={ref} className="relative w-full">
       <div onClick={() => setOpen(true)}>
-        <Input label={label} value={value} />
+        <Input label={label} value={value} disabled={disabled} prefix={renderPrefix()} />
       </div>
       {open && (
         <div className="absolute max-h-[200px] w-full flex flex-col divide-y divide-border border-solid border  border-border-hover rounded-b-input overflow-y-auto">
           {options.map((option) => (
             <div
               key={option}
-              className="min-h-[42px] flex items-center cursor-pointer px-2 bg-background-white z-10 last:rounded-bl-input last:rounded-br-input"
+              title={option}
+              className="min-h-[42px] flex items-center cursor-pointer px-2 bg-background-white truncate z-10 last:rounded-bl-input last:rounded-br-input"
               onClick={() => handleSelect(option)}
             >
               {option}
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,22 +1,33 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import clsx from 'clsx';
 
 interface Props {
   className?: string;
   label: string;
   value: string;
+  disabled?: boolean;
+  prefix?: ReactNode;
   onChange?: (value: string) => void;
 }
 
-const Input: FC<Props> = ({ className, label, value, onChange }) => {
+const Input: FC<Props> = ({ className, label, value, disabled, prefix, onChange }) => {
   return (
     <div className={clsx(className, 'relative w-full')}>
-      <input
-        value={value}
-        className="w-full h-full rounded-input border border-border p-3 leading-normal outline-none focus:border-border-hover placeholder:text-text-grey"
-        placeholder={label}
-        onChange={(e) => onChange?.(e.target.value)}
-      />
+      <div
+        className={clsx(
+          disabled && 'bg-border',
+          'flex justify-between items-center rounded-input border border-border leading-normal focus:border-border-hover placeholder:text-text-grey'
+        )}
+      >
+        <input
+          value={value}
+          className="h-full w-full p-3 outline-none"
+          placeholder={label}
+          disabled={disabled}
+          onChange={(e) => onChange?.(e.target.value)}
+        />
+        {prefix}
+      </div>
     </div>
   );
 };
